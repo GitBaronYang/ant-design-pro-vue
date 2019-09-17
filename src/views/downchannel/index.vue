fec799ffa9770ce4a621b41807e5a4a67617190a
yangfei <yangfei@castianta.com>
2019-09-17 19:45:34 +0800

    annotation uncorrelated router

--- src/views/downchannel/index.vue
+++ src/views/downchannel/index.vue
@@ -1,38 +1,114 @@
 <template>
   <div>
     <a-row>
-      <a-card title="TM">
+      <a-card title="遥测（TM）">
         <a href="#" slot="extra">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
+        <a-row>
+          <a-col :span="12">
+            <a-upload
+              name="file"
+              :multiple="true"
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              :headers="headers"
+              @change="handleChange"
+            >
+              <a-button>
+                <a-icon type="upload" />Click to Upload
+              </a-button>
+            </a-upload>
+          </a-col>
+          <a-col :span="12">
+            <a-button type="primary">发送</a-button>
+          </a-col>
+        </a-row>
       </a-card>
     </a-row>
     <a-row>
       <a-card title="图像">
         <a href="#" slot="extra">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
+        <a-row>
+          <a-col :span="12">
+            <a-upload
+              name="avatar"
+              listType="picture-card"
+              class="avatar-uploader"
+              :showUploadList="false"
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              :beforeUpload="beforeUpload"
+              @change="handleChangeImage"
+            >
+              <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+              <div v-else>
+                <a-icon :type="loading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">Upload</div>
+              </div>
+            </a-upload>
+          </a-col>
+          <a-col :span="12">
+            <a-button type="primary">发送</a-button>
+          </a-col>
+        </a-row>
       </a-card>
     </a-row>
     <a-row>
       <a-card title="TC环回">
         <a href="#" slot="extra">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
+        <p>TC环回和上行通道TC绑定：收到TC后将TC码返回给发送方</p>
       </a-card>
     </a-row>
   </div>
 </template>
+<script>
+function getBase64 (img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+export default {
+  data () {
+    return {
+      headers: {
+        authorization: 'authorization-text'
+      },
+      loading: false,
+      imageUrl: ''
+    }
+  },
+  methods: {
+    handleChange (info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList)
+      }
+      if (info.file.status === 'done') {
+        this.$message.success(`${info.file.name} file uploaded successfully`)
+      } else if (info.file.status === 'error') {
+        this.$message.error(`${info.file.name} file upload failed.`)
+      }
+    },
+    handleChangeImage (info) {
+      if (info.file.status === 'uploading') {
+        this.loading = true
+        return
+      }
+      if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, imageUrl => {
+          this.imageUrl = imageUrl
+          this.loading = false
+        })
+      }
+    },
+    beforeUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        this.$message.error('You can only upload JPG file!')
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isLt2M) {
+        this.$message.error('Image must smaller than 2MB!')
+      }
+      return isJPG && isLt2M
+    }
+  }
+}
+</script>
--- src/views/uplinkchannel/index.vue
+++ src/views/uplinkchannel/index.vue
@@ -3,13 +3,8 @@
     <a-row>
       <a-card title="遥控看板">
         <a href="#" slot="extra">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
+        <a-table bordered :dataSource="dataSource" :columns="columns">
+        </a-table>
       </a-card>
     </a-row>
     <a-row>
@@ -25,3 +20,82 @@
     </a-row>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      dataSource: [
+        {
+          key: '0',
+          time: 'Edward King 0',
+          dataIndex: 'London, Park Lane no. 0'
+        },
+        {
+          key: '1',
+          time: 'Edward King 1',
+          contents: 'London, Park Lane no. 1'
+        }
+      ],
+      count: 2,
+      columns: [
+        {
+          title: 'time',
+          dataIndex: 'time',
+          width: '15%',
+          scopedSlots: { customRender: 'time' }
+        },
+        {
+          title: 'contents',
+          dataIndex: 'contents'
+        }
+      ]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+.editable-cell {
+  position: relative;
+}
+
+.editable-cell-input-wrapper,
+.editable-cell-text-wrapper {
+  padding-right: 24px;
+}
+
+.editable-cell-text-wrapper {
+  padding: 5px 24px 5px 5px;
+}
+
+.editable-cell-icon,
+.editable-cell-icon-check {
+  position: absolute;
+  right: 0;
+  width: 20px;
+  cursor: pointer;
+}
+
+.editable-cell-icon {
+  line-height: 18px;
+  display: none;
+}
+
+.editable-cell-icon-check {
+  line-height: 28px;
+}
+
+.editable-cell:hover .editable-cell-icon {
+  display: inline-block;
+}
+
+.editable-cell-icon:hover,
+.editable-cell-icon-check:hover {
+  color: #108ee9;
+}
+
+.editable-add-btn {
+  margin-bottom: 8px;
+}
+</style>
